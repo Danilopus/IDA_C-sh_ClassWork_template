9e4f8130f06eb5f33ac10cfb32c20c82295c1386
Ишутинов Данил <gladius@inbox.ru>
2023-09-29 15:47:37 +0500

    Add project files.

---
+++ IDA_C-sh_ClassWork_template/!_Program.cs
@@ -0,0 +1,33 @@
+﻿// ClassWork template 1.2 // date: 29.09.2023
+
+using Service;
+using System;
+using System.Linq.Expressions;
+using System.Text;
+
+
+
+// ClassWork XX : [C sharp] DD.MM.YYYY --------------------------------
+
+namespace IDA_C_sh_ClassWork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Task_1();
+            // Task_2();
+            // Task_3();
+
+
+            // Console.ReadKey();
+        }
+
+        public static void Task_1() { }
+        public static void Task_2() { }
+        public static void Task_3() { }
+
+    } // class Programm
+}// namespace
+
